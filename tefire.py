a2c24906b6b72288f0518af5763818818ad28b9d
Jimmy Le <le.jimmy91@gmail.com>
2021-09-09 21:49:38 -0500

    Initial commit for anchor and luna staking options

--- tefire.py
+++ tefire.py
@@ -12,11 +12,21 @@ st.sidebar.title("Inputs")
 ust_deposited = st.sidebar.slider(
     "UST Deposit in Anchor Earn",
     min_value=10,
-    max_value=1000,
-    value=100,
-    step=1,
+    max_value=10000000,
+    value=10000,
+    step=100,
     format="$%.2f",
     help="What do you think the future price of Luna will be?",
+)
+
+# Anchor Earn APY
+earn_apy = st.sidebar.slider(
+    "Anchor Earn APY",
+    min_value=0.0,
+    max_value=40.0,
+    value=19.5,
+    step=0.01,
+    help="What do you think future Anchor Earn APY will be?",
 )
 
 # Luna Price
@@ -25,7 +35,7 @@ luna_price = st.sidebar.slider(
     min_value=10,
     max_value=1000,
     value=100,
-    step=1,
+    step=10,
     format="$%.2f",
     help="What do you think the future price of Luna will be?",
 )
@@ -63,14 +73,28 @@ pluna_yluna_ratio = st.sidebar.slider(
 
 st.header("Anchor Protocol")
 st.write("Scenario for monthly cashflow via Anchor Earn.")
+fv = ust_deposited * earn_apy / 100 + ust_deposited
+fv_ust_deposited = fv / ust_deposited
+int_periods = fv_ust_deposited / 5256000
+epoch_interest = pow(fv_ust_deposited, int_periods) - 1
+usd_day = ust_deposited * epoch_interest * 14400
+usd_month = ust_deposited * epoch_interest * 438000
+st.write(f"Interest earned per day: ${usd_day:,.2f}.")
+st.write(f"Interest earned per month: ${usd_month:,.2f}.")
+
 
 st.header("Luna Staking")
 st.write("Scenario for monthly cashflow via Staking Luna.")
+staking_rewards = luna_staked * luna_apr / 100
+staking_rewards_usd = staking_rewards * luna_price
+monthly_usd = staking_rewards_usd / 12
+st.write(f"Staking Rewards: {staking_rewards:.2f} Luna")
+st.write(f"Staking Rewards in USD: ${staking_rewards_usd:,.2f}")
+st.write(f"Monthly Rewards in USD: ${monthly_usd:,.2f}")
+
 
 st.header("Prism Protocol")
 st.write("Scenario for decomposing annual Luna staking rewards into pLuna-yLuna.")
-
-staking_rewards = luna_staked * luna_apr / 100
 total_yluna = staking_rewards / (1 - pluna_yluna_ratio)
 annual_cash_flow = total_yluna * luna_price
 
