71f0abb7c5e9305624c7321ce5ec72cb8eb7d27e
Jimmy Le <le.jimmy91@gmail.com>
2021-09-10 11:08:58 -0500

    Update narrative.

--- tefire.py
+++ tefire.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 # Basic setup and app layout
 # st.set_page_config(layout="wide")
@@ -9,13 +10,12 @@ st.title("TeFIRE Calculator")
 st.sidebar.title("Inputs")
 
 # Anchor Earn
-ust_deposited = st.sidebar.slider(
-    "UST Deposit in Anchor Earn",
-    min_value=10,
-    max_value=10000000,
-    value=10000,
-    step=100,
-    format="$%.2f",
+ust_deposited = st.sidebar.number_input(
+    "Anchor Earn Deposit in UST",
+    min_value=1000,
+    max_value=10_000_000_000,
+    value=1_000_000,
+    format="%.2d",
     help="What do you think the future price of Luna will be?",
 )
 
@@ -44,15 +44,15 @@ luna_price = st.sidebar.slider(
 luna_staked = st.sidebar.slider(
     "Staked Luna",
     min_value=100,
-    max_value=20000,
-    value=1000,
+    max_value=50_000,
+    value=10_000,
     step=100,
     help="How much Luna do you plan to stake?",
 )
 
 # Staking APR
 luna_apr = st.sidebar.slider(
-    "Luna Rewards APR",
+    "Staking Return",
     min_value=1,
     max_value=50,
     value=10,
@@ -66,41 +66,104 @@ pluna_yluna_ratio = st.sidebar.slider(
     "pLuna to yLuna Ratio",
     min_value=0.0,
     max_value=1.0,
-    value=0.85,
+    value=0.90,
     step=0.01,
     help="What do you pLuna will be worth relative to yLuna?",
 )
 
-st.header("Anchor Protocol")
-st.write("Scenario for monthly cashflow via Anchor Earn.")
-fv = ust_deposited * earn_apy / 100 + ust_deposited
-fv_ust_deposited = fv / ust_deposited
-int_periods = fv_ust_deposited / 5256000
-epoch_interest = pow(fv_ust_deposited, int_periods) - 1
-usd_day = ust_deposited * epoch_interest * 14400
-usd_month = ust_deposited * epoch_interest * 438000
-st.write(f"Interest earned per day: ${usd_day:,.2f}.")
-st.write(f"Interest earned per month: ${usd_month:,.2f}.")
+st.markdown(
+    """
+    Recently, I conducted a poll on [Twitter](https://twitter.com/lejimmy/status/1435017886226124803?s=20) asking the community about "making it" in crypto and what that might translate to in terms of monthly income.
+    
+    In this thread, I explored a few ways that you could generate this monthly income in the Terra Luna ecosystem.
 
+    Of course, there were many assumptions made.  With this tool, you can play around with your projections and consider your financial goals on your journey to "making it."
 
-st.header("Luna Staking")
-st.write("Scenario for monthly cashflow via Staking Luna.")
+    """
+)
+
+st.warning(
+    "Disclaimer: This tool was created by a smooth brain.  Do not consider such information as investment advice."
+)
+
+st.markdown("## **Anchor Protocol**")
+
+annual_interest = ust_deposited * earn_apy / 100
+monthly_interest = annual_interest / 12
+
+st.markdown(
+    f"""
+    Depositing UST into Anchor Earn is the simplest path to a stable income.
+
+    A **${ust_deposited:,.2f} UST deposit**, at **{earn_apy}% APY** will earn an estimated **${annual_interest:,.2f} per year** or roughly **${monthly_interest:,.2f} per month**.
+    
+    If you are withdrawing your monthly income every month, you would expect a little less as the funds are not compounding for the entire year.  To be extremely precise, you would want to calculate the USD gained per block and only compound for the periods between withdrawal.
+    """
+)
+
+st.markdown("## **Staking Luna**")
 staking_rewards = luna_staked * luna_apr / 100
 staking_rewards_usd = staking_rewards * luna_price
 monthly_usd = staking_rewards_usd / 12
-st.write(f"Staking Rewards: {staking_rewards:.2f} Luna")
-st.write(f"Staking Rewards in USD: ${staking_rewards_usd:,.2f}")
-st.write(f"Monthly Rewards in USD: ${monthly_usd:,.2f}")
+
+st.markdown(
+    f"""
+    By staking or delegating your Luna to a validator, you earn staking rewards paid in Luna, Terra stablecoins, and protocol airdrops.
+
+    Staking rewards are projected to increase to double digit APRs after the Col-5 mainnet upgrade.
+
+    At a **{luna_apr}% APR** staking return, **{luna_staked:,} Luna** staked, valued at **${luna_staked*luna_price:,.2f}**, will earn an equivalent of **{staking_rewards:,.2f} Luna** per year.
+
+    If you sold all of the staking rewards at **${luna_price} per Luna**, you would earn a total of **${staking_rewards_usd:,.2f} per year** or roughly **${staking_rewards_usd/12:,.2f} per month**.
+     """
+)
 
 
-st.header("Prism Protocol")
-st.write("Scenario for decomposing annual Luna staking rewards into pLuna-yLuna.")
+st.markdown("## **Prism Protocol**")
 total_yluna = staking_rewards / (1 - pluna_yluna_ratio)
-annual_cash_flow = total_yluna * luna_price
+annual_yluna_rewards_usd = total_yluna * luna_price * luna_apr / 100
 
-st.write(f"Staking Rewards: {staking_rewards:.2f} Luna")
-st.write(f"Decomposed Principal Tokens to Sell: {staking_rewards:.2f} pLuna")
-st.write(f"Decomposed Yield Tokens: {staking_rewards:.2f} yLuna")
-st.write(f"Total Yield Tokens: {total_yluna:.2f} yLuna")
-st.write(f"Annual Cashflow: ${annual_cash_flow:,.2f}")
-st.write(f"Monthly Cashflow: ${annual_cash_flow/12:,.2f}")
+st.markdown(
+    f"""
+    With Prism, you can decompose your **staking rewards of {staking_rewards:,.2f} Luna** into a principal and yield component.  This allows you to speculate only on the price of Luna or only on the cash flow that your Luna may generate.
+
+    If the market values these components at a **{pluna_yluna_ratio} pLuna to yLuna ratio**, pLuna tokens can be swapped for a total of **{total_yluna:,.2f} yLuna** tokens.
+
+    Over the next year, the yLuna will generate **${annual_yluna_rewards_usd:,.2f}** in rewards or roughly **${annual_yluna_rewards_usd/12:,.2f} per month.**
+
+    
+    """
+)
+
+prism_staking_diff = annual_yluna_rewards_usd / 12 - staking_rewards_usd / 12
+
+if prism_staking_diff > 0:
+    st.markdown(
+        f"In this scenario, you would make **${prism_staking_diff:,.2f} more per month** by decomposing and swapping into yLuna versus selling your staking rewards outright."
+    )
+elif prism_staking_diff < 0:
+    st.markdown(
+        f"In this scenario, you would make **${abs(prism_staking_diff):,.2f} less per month** by decomposing and swapping into yLuna versus selling your staking rewards outright."
+    )
+
+st.markdown(
+    """
+    Of course this gets to become complicated and nuanced very quickly when you introduce the 3, 6, 9, and 12 month expiration dates of these components.
+
+    The yLuna expiry dates may overlap and the market may value them at varying rates.  You could potentially swap yLuna expiring in Dec 2022 half way through the term with a newly yLuna contract expiring in Dec 2023. 
+
+    """
+)
+
+st.markdown("### **Closing Thoughts**")
+st.markdown(
+    f"""
+    I hope this was useful, perhaps a bit insightful, and as always, wagmi.
+
+    Feel free to reach out to me on Twitter [@lejimmy](https://twitter.com/lejimmy) if you have any questions or feedback.
+     
+    If you're included to contribute directly, please fork the [Github Repo here](https://github.com/lejimmy/streamlit-dashboards).
+
+    If you'd like to support more of this type of work, consider contributing to my wallet here: *terra1m3sl9qea92km6mqm02yqxfygn8g9acl8wzj6x7*
+    """
+)
