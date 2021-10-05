0b2499d0a4a2f07bb7941814163b1d61e4fe2587
Jimmy Le <le.jimmy91@gmail.com>
2021-10-05 08:26:37 -0500

    Scrape first 3 pages

--- gp.py
+++ gp.py
@@ -10,17 +10,25 @@ from PIL import Image
 # st.set_page_config(layout="wide")
 st.title("Galactic Punks")
 
+responses = []
+for i in range(3):
+    response = requests.get(
+        url=f"https://randomearth.io/api/items?collection_addr=terra103z9cnqm8psy0nyxqtugg6m7xnwvlkqdzm4s4k&sort=price.asc&page={i}&on_sale=1"
+    )
+    responses.append(response)
 
-response = requests.get(
-    url="https://randomearth.io/api/items?collection_addr=terra103z9cnqm8psy0nyxqtugg6m7xnwvlkqdzm4s4k&sort=price.asc&page=1&on_sale=1"
-)
+dfs = pd.DataFrame()
+for response in responses:
+    df = pd.DataFrame(response.json()["items"])
+    dfs = pd.concat([dfs, df])
 
-df = pd.DataFrame(response.json()["items"])
+dfs.reset_index(drop=True, inplace=True)
+
 df_traits = pd.DataFrame([flatten(d) for d in df["traits"].to_list()])
 df_rarity = pd.read_csv("GP Rarity Calculator.csv")
 
 df_merge = pd.concat(
-    [df, df_traits],
+    [dfs, df_traits],
     axis=1,
 )
 
