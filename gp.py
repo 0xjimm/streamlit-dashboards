58037349dd588c6e60f7488b5ca122ce665b3eeb
Jimmy Le <le.jimmy91@gmail.com>
2021-10-05 10:27:14 -0500

    Async image requests

--- gp.py
+++ gp.py
@@ -1,12 +1,11 @@
 import streamlit as st
-import plotly.express as px
-import statsmodels.api as sm
 import pandas as pd
-import json
 import requests
 from flatten_json import flatten
 from PIL import Image
 from io import BytesIO
+import httpx
+import asyncio
 
 # st.set_page_config(layout="wide")
 st.title("Galactic Punks")
@@ -43,18 +42,34 @@ col1, col2, col3 = st.columns(3)
 
 df_merge.sort_values(by="ranking", ascending=True, inplace=True)
 
-dfs.reset_index(drop=True, inplace=True)
+df_merge.reset_index(drop=True, inplace=True)
 
-for i, row in df_merge.iterrows():
 
-    image = Image.open(BytesIO(requests.get(row["src"]).content))
+async def get_images():
+    async with httpx.AsyncClient() as client:
 
-    cols = st.columns(5)
+        for i, row in df_merge.iterrows():
+            url = row["src"]
+            resp = await client.get(url)
+            df_merge.loc[i, "image"] = resp.content
 
-    cols[0].image(image)
-    cols[1].write(row["name"])
-    cols[1].write(f"Price: {row['price'] / 1_000_000} LUNA")
-    cols[2].write(f'Ranking: {row["ranking"]}')
-    cols[2].markdown(
-        f'[Link](https://randomearth.io/items/terra103z9cnqm8psy0nyxqtugg6m7xnwvlkqdzm4s4k_{row["token_id_x"]})'
-    )
+
+asyncio.run(get_images())
+
+
+def display_table():
+    for i, row in df_merge.iterrows():
+
+        cols = st.columns(5)
+        cols[0].image(Image.open(BytesIO(row["image"])))
+        cols[1].write(row["name"])
+        cols[1].write(f"Price: {row['price'] / 1_000_000} LUNA")
+        cols[2].write(f'Ranking: {row["ranking"]}')
+        cols[2].markdown(
+            f'[Link](https://randomearth.io/items/terra103z9cnqm8psy0nyxqtugg6m7xnwvlkqdzm4s4k_{row["token_id_x"]})'
+        )
+
+    pass
+
+
+display_table()
